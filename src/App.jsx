856af2148f15a5f2dbed243daaa1964c489cb6f5
adeeb-fawwaz <adeebny2005@gmail.com>
2026-05-26 19:07:22 +0530

    feat: complete professional portfolio layout and resolve submodule index constraints

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Hero from './components/Hero';
+
+export default function App() {
+  return (
+    <div>
+      {/* This automatically pulls your Hero block onto the main screen! */}
+      <Hero />
+    </div>
+  );
+}
